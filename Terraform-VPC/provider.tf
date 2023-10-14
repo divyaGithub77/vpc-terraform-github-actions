874fdb328eaea4f5048dd9215b8c008fe4ac839b
divyaGithub77 <preciselymine77@gmail.com>
2023-10-14 23:06:49 +0530

    commit

--- Terraform-VPC/provider.tf
+++ Terraform-VPC/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "terraform-remote-backend-s3"
+    bucket = "terraform-remote-backend-s301"
     key    = "dev/terraform.tfstate"
     region = "ap-south-1"
   }
